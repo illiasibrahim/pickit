2a1255e74a504985aed3a2fd17025de683e8b030
illias <illiasibrahim6@gmail.com>
2021-10-06 11:06:21 +0530

    collectstatic heroku error fix 1

--- pickit/settings.py
+++ pickit/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
